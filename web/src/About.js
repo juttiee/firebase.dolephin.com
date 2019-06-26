eb94dabee762214033a0b8c03a3afc38b4890915
Joseph Lee <juttiee@gmail.com>
2019-06-26 14:50:43 +0900

    edit demo app

---
+++ web/src/About.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <p><img src='./images/CI_256.png' alt="CI" /></p>
+        <p>Dolephin.com</p>
+      </div>
+    );
+  }
+}
